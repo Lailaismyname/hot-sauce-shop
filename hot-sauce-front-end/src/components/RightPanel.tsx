0a61321485ee242387ae43df494598e67b4ef83b
Laila <91194082+Lailaismyname@users.noreply.github.com>
2024-06-27 22:53:47 +0200

    removed mysterious semicolon

--- hot-sauce-front-end/src/components/RightPanel.tsx
+++ hot-sauce-front-end/src/components/RightPanel.tsx
@@ -1,7 +1,7 @@
 export const RightPanel = ({ children }) => {
   return (
     <>
-      <ul className="col-span-5 grid grid-cols-3 gap-8 mt-10">{children}</ul>;
+      <ul className="col-span-5 grid grid-cols-3 gap-8 mt-10">{children}</ul>
     </>
   );
 };
